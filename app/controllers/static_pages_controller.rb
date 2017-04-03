96fd348b8d029369df8c1dc61abd2274b01e0999
Eli Salm <salmeli@grinnell.edu>
2017-04-03 19:45:22 +0000

    Added Front End Search Page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,8 @@ class StaticPagesController < ApplicationController
   
   def about
   end
+  
+  def search
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   get 'static_pages/about'
-
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/search'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
+  root 'static_pages#search'
   
   #devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 
