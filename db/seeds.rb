a4c6c7f13c892e0ce87af2fbdb2b07e5b31bafac
Harriet Zucker <harriet.ponder@gmail.com>
2017-04-10 19:06:06 +0000

    Fixing testing Branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
 
-User.create({ :email => "rebelsky@grinnell.edu", :password => "mayflower" })
+# residents = CSV.open("Workbook1.csv", :headers => true)
+# residents_data = CSV.parse(residents)
+# #arr_of_arrs = CSV.parse("Workbook1.csv")
+# p residents_data[0]
+
+# residents_data.each do |row|
+#     p row
+#     #data = row.split(',')
+#     #row.fields.to_csv( options )
+    
+#     data = row.split(',')
+#     Residents.create(data[0], data[1], data[2], data[3])
+# end
+
+# p Residents.column_names
+
+# csv_data = File.read('Workbook1.csv')
+# csv = CSV.parse(csv_data, :headers => true)
+# csv.each do |row|
+#   Residents.create!(row.to_hash)
+# end
+
+#Frizbee.create!(:name => 'Yetter')
+ 
+ csv_data = File.read('Workbook1.csv')
+ csv = CSV.parse(csv_data, :headers => true)
+ csv.each do |row|
+     row = row.to_hash.with_indifferent_access
+     bob = row.to_hash.symbolize_keys
+     Profile.create!(bob)
+ end
+
+#Profile.create!(:first_name=>"Yetter", :last_name=>"Bob")
