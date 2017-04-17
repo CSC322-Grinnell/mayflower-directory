86bc9dc30382d402effc41d994a62479acd7ddda
Jong Hoon Bae <baejongh@grinnell.edu>
2017-04-17 19:05:48 +0000

    updating residents works

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,22 @@
 class ProfilesController < ApplicationController
   def new
   end
+  
+  def edit
+    @user = Profile.find(params[:id])
+  end
+  
+  def profile_params
+      params.require(:profile).permit(:first_name, :last_name)
+  end
+   
+  def update
+    @user = Profile.find(params[:id])
+    if @user.update_attributes(profile_params)
+      flash[:success] = "Profile updated"
+      redirect_to '/static_pages/home'
+    else
+      render 'edit'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/search'
+  resources :profiles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
