6673d9fb15e95e5b997193072ab4391d568718b7
Jong Hoon Bae <baejongh@grinnell.edu>
2017-04-21 19:42:51 +0000

    edit and new profile working

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,8 @@
 class ProfilesController < ApplicationController
-  #before_action :logged_in_admin, only: [:edit, :update, :create, :index]
+  before_action :logged_in_admin, only: [:edit, :update, :create, :index]
+  
   def new
-    @profile = Profile.new
+    @user = Profile.new
   end
   
   def edit
@@ -9,24 +10,28 @@ class ProfilesController < ApplicationController
   end
   
   def profile_params
-      params.require(:profile).permit(:first_name, :last_name)
+    params.require(:profile).permit(:first_name, :last_name, :nickname, :landline, :cell, :email, :address, :neighborhood, :spouse)
   end
    
   def update
     @user = Profile.find(params[:id])
     if @user.update_attributes(profile_params)
       flash[:success] = "Profile updated"
-      redirect_to '/static_pages/home'
+      redirect_to '/static_pages/search'
     else
       render 'edit'
     end
   end
   
   def create
-    @user = Profile.new(params[:id])
-    if @user.save(profile_params)
+    puts "*** CREATING A NEW USER ****"
+    puts profile_params
+    @user = Profile.new(profile_params)
+    puts "User is "
+    puts @user.to_s
+    if @user.save
       flash[:notice] = "profile sucessfully added"
-      redirect_to @user
+      redirect_to '/profiles/' + @user[:id].to_s
     else 
       flash[:notice] = "there was a problem creating the new profile"
       render action 'profile/new'
@@ -34,20 +39,21 @@ class ProfilesController < ApplicationController
   end
   
   def index
-    @search = Profile.search(params[:q])
+    @search = Profile.search(params[:id])
     @profiles = @search.result
     @user = Profile.all
   end 
   
   # Confirms a logged-in user as admin.
-  # def logged_in_admin
-  #   unless current_user.admin
-  #     flash[:danger] = "Please log in as admin."
-  #     redirect_to '/static_pages/home'
-  #   end
-  # end
+   def logged_in_admin
+     unless current_user.admin
+       flash[:danger] = "Please log in as admin."
+       redirect_to '/static_pages/home'
+     end
+   end
   
  def show
    @user = Profile.find(params[:id])
  end
+ 
 end
