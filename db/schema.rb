ef86bd21ed369adfe01f976698078b0b8445fe1f
Joshua Tibatemwa <tibatemw@grinnell.edu>
2017-10-24 15:50:41 +0000

    Fixed issues with log out and search links appearing even though you are not logged in

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20170426181243) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
-    t.boolean  "newUser",                 default: true
+    t.boolean  "newUser",                default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
