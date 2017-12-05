cd205998535fff7ca32581ee74e071764d3ce03f
Addison Gould <gouldadd@grinnell.edu>
2017-12-05 16:12:21 +0000

    fixing image displaying, need to update images folder on AWS

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -17,8 +17,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
   
   def cache_dir
-    'images-new'
-    #used to be 'images', but Mayflower group gave us new images
+    'images'
   end
   
   #stuff in comments can be commented out if we need them later
