248a864865081659d8b38c3110ccfd421add5825
Rachel Swoap <swoaprac@grinnell.edu>
2018-03-07 21:03:19 +0000

    updated gemfile

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY'],                        # required
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required
     region:                ENV['AWS_DEFAULT_REGION'],                  # optional, defaults to 'us-east-1'
   }
