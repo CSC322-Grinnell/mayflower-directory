02f16172fcdc9f6e6783dd1f73a7996c8a329150
Pat Min <minpatri@grinnell.edu>
2018-04-04 19:13:24 +0000

    normalize phone

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,8 @@ class Profile < ActiveRecord::Base
     before_create :normalize_phone
     
     def normalize_phone
-       self.landline.delete('^0-9')
+       self.landline = self.landline.delete('^0-9')
+       #Future work: Make a test suite (test cases of various formats of phone numbers), add hyphens/parentheses, and check for 7-digit or 10-digit number)
+       #Also, check if there are any phone numbers with country code (+1, etc.) and consider handling that case
     end
 end
