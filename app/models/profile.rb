694fe54f94b8c9ba01c85682bd122db0f936899e
Pat Min <minpatri@grinnell.edu>
2018-04-04 01:51:10 +0000

    Started normalize-phone method

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,9 @@
 class Profile < ActiveRecord::Base
-  mount_uploader :avatar, AvatarUploader
+    mount_uploader :avatar, AvatarUploader
+    
+    before_create :normalize_phone
+    
+    def normalize_phone
+       self.landline.delete('^0-9')
+    end
 end
