dab9d1946215d8a2a7cade95d8e1fa6f5a3917bb
Rachel Swoap <swoaprac@grinnell.edu>
2018-04-30 20:21:26 +0000

    added import CSV to create users page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  require 'csv'
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      User.create! row.to_hash
+    end
+  end
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :secure_validatable
 end
