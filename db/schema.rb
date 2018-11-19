d7b2ae5ea5d2675a03a757f40f2b8c497dad0292
Nhi Ngo <ngonhi@grinnell.edu>
2018-11-19 10:43:29 -0600

    Revert "Draft biography"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181116224603) do
+ActiveRecord::Schema.define(version: 20171102005856) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "first_name"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20181116224603) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "avatar"
-    t.string   "biography"
   end
 
   create_table "users", force: :cascade do |t|
