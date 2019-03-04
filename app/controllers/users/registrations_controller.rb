1d9d5e9f12aff0cd2bf2381f4339dd0bd0f67fd9
tranle <lequytra@grinnell.edu>
2019-03-04 17:32:01 +0000

    FINISHED USER NAMESgit status!

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,7 +38,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
    def create
         if params[:admin]
-            puts 'hi'
+            puts "hi\n\n"
         end
      super
    end
@@ -71,10 +71,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-   def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:admin])
-   end
-
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:admin])
+  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+    user.permit(:id, :name, :email, :password, :password_confirmation)
+    end
+  end
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
