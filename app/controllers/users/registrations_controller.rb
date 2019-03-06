e35b1417747be2ce13b8af57d3570c1a3f77f88f
John Lennon <lennonjo@grinnell.edu>
2019-03-06 17:11:29 +0000

    Fixed some of the easier things from the comments

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -69,9 +69,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:admin])
-  # end
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up) do |user|
     user.permit(:id, :name, :email, :password, :password_confirmation)
