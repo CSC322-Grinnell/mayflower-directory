f6997391afd3ebb8efd7a47ffc525d80f8dc977f
tranle <lequytra@grinnell.edu>
2019-04-02 14:25:06 +0000

    Delete dead code in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
-  def hello
-    render html: "Kathryn Yetter"
-  end
-  
-  def yourein
-    render html: "Jong Hoon Bae"
-  end
+
 end
