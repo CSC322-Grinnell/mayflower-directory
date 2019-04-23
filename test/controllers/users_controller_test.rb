5b8549799c594369fd61474c704d95cad2383a41
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2019-04-23 14:48:52 -0500

    Add tests to limit permissions for regular users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,38 +3,44 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  # TODO: regular users shouldn't be able to do anything
-
-  test "should redirect to users page after deleting a user" do
-    login_as(users(:admin))
-    delete user_path(users(:normal))
-
-    assert_response :redirect
-    follow_redirect!
-    assert_equal users_path, path
+  setup do
+    @foo_attributes = {
+      name: "Foo",
+      email: "foobarbaz@bartell.com",
+      admin: true,
+    }
   end
 
   test "admin should be able to update user info" do
     login_as(users(:admin))
     user = users(:normal)
-    foo_attributes = {
-      name: "Foo",
-      email: "foobarbaz@bartell.com",
-      admin: true,
-    }
 
     get edit_info_user_path(user)
     assert_select "form[action='#{user_path(user)}']" do
-      foo_attributes.each_key do |attribute|
+      @foo_attributes.each_key do |attribute|
         assert_select "input[name='user[#{attribute}]']"
       end
     end
 
-    put user_path(user), params: { user: foo_attributes }
+    put user_path(user), params: { user: @foo_attributes }
     user.reload
 
-    foo_attributes.each do |attribute, value|
+    @foo_attributes.each do |attribute, value|
       assert_equal value, user[attribute]
+    end
+  end
+
+  test "regular users should not be able to update user info" do
+    login_as(users(:normal))
+    user = users(:other_normal)
+
+    old_attributes = user.attributes
+
+    put user_path(user), params: { user: @foo_attributes }
+    user.reload
+
+    user.attributes.each do |attribute, value|
+      assert_equal old_attributes[attribute], value
     end
   end
 
@@ -47,18 +53,29 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_equal users_path, path
   end
 
-  #Error
-  #  test "should update password" do
-  #    login_as(users(:admin))
-  #    users(:normal).update({password: "Passw0rd!!", password_confirmation: "Passw0rd!!"})
-  #    assert_match(users(:normal).password, "Passw0rd!!")
-  #  end
+  test "admin should be able to delete a user" do
+    login_as(users(:admin))
 
-    #This is a failure
-  # test "user should be nil after deleting" do
-  #   login_as(users(:admin))
-  #   users(:normal).destroy
-  #   assert_nil(users(:normal))
-  # end
+    assert_difference -> { User.all.count }, -1 do
+      delete user_path(users(:normal))
+    end
+  end
+
+  test "regular users should not be able to delete a user" do
+    login_as(users(:normal))
+
+    assert_no_difference -> { User.all.count } do
+      delete user_path(users(:other_normal))
+    end
+  end
+
+  test "should redirect to users page after deleting a user" do
+    login_as(users(:admin))
+    delete user_path(users(:normal))
+
+    assert_response :redirect
+    follow_redirect!
+    assert_equal users_path, path
+  end
 
 end
