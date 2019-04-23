7b795d0403f09bdbac3657bef7cf18142d53ea10
grinnelldennis <chanchun@grinnell.edu>
2019-04-23 18:29:56 +0000

    add tests for controller

--- test/controllers/departments_controller_test.rb
+++ test/controllers/departments_controller_test.rb
@@ -2,6 +2,7 @@
 
 class DepartmentsControllerTest < ActionDispatch::IntegrationTest
   # include Devise::Test::IntegrationHelpers
+  # ref: https://guides.rubyonrails.org/v5.0/testing.html#the-low-down-on-fixtures
 
   setup do
     @dea = {
@@ -23,7 +24,57 @@ class DepartmentsControllerTest < ActionDispatch::IntegrationTest
     assert_response :sucess
   end
 
+  test "should show all departments on landing page as user" do
 
+  end
+
+  test "should show all departments on landing page as admin" do
+
+  end
+
+  test "should allow admin create new department" do
+
+  end
+
+  test "should not allow user create new department" do
+
+  end
+
+  test "should allow admin edit new department" do
+
+  end
+
+  test "should not allow user edit new department" do
+
+  end
+
+  test "should allow admin remove department" do
+
+  end
+
+  test "should not allow user remove department" do
+
+  end
+
+  test "should redirect admin to department after edit" do
+
+  end
+
+  test "should redirect admin to department after new" do
+
+  end
+
+  test "should redirect admin to department after remove" do
+
+  end
+
+  test "should edit page successfully as amin" do
+
+  end
+
+  test "should load each departmennt correctly" do
+
+  end
 
 
 
