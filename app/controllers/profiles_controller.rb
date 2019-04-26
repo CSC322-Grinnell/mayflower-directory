3f21083eefceb33d8a6e10bcc574cb97d9ec4f3c
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2019-04-25 21:54:29 -0500

    Remove vestigial image querying helpers

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -84,42 +84,4 @@ class ProfilesController < ApplicationController
         :neighborhood, :spouse, :biography, :avatar
       )
     end
-
-    def get_bucket
-      s3 = Aws::S3::Resource.new(
-        region: 'us-east-2',
-        credentials: Aws::Credentials.new(
-          ENV['AWS_ACCESS_KEY_ID'],
-          ENV['AWS_SECRET_ACCESS_KEY']
-        )
-      )
-
-      s3.bucket('mayflower-data')
-    end
-
-    def profile_image(profile, bucket)
-      folder_name = "images"
-      base_filename = "#{profile.last_name}, #{profile.first_name}"
-      png_filename = "#{base_filename}.png"
-      jpg_filename = "#{base_filename}.jpg"
-      default_url = view_context.image_url("Mayflower_Default_Photo.jpg")
-
-      begin
-        if not profile.avatar.file.nil?
-          return profile.avatar.url
-        elsif bucket.object(File.join(folder_name, jpg_filename)).exists?
-          uploader = AvatarUploader.new
-          uploader.retrieve_from_store!(jpg_filename)
-          return uploader.url
-        elsif bucket.object(File.join(folder_name, png_filename)).exists?
-          uploader = AvatarUploader.new
-          uploader.retrieve_from_store!(png_filename)
-          return uploader.url
-        else
-          return default_url
-        end
-      rescue Aws::S3::Errors::BadRequest
-        return default_url
-      end
-    end
 end
