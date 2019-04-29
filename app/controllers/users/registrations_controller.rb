78e33ad8c1cb97f4137635c78dd1ae80b9d7eacc
John Lennon <lennonjo@grinnell.edu>
2019-04-29 15:30:08 +0000

    Cleaned up registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -25,21 +25,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 
-  # POST /resource
-   def create
-     super
-   end
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-   def update
-     super
-   end
-
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
