58bfaa6a13c51eeac2a33772ca936963613b9065
tranle <lequytra@grinnell.edu>
2019-04-30 17:00:14 +0000

    Update relationship

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,8 +1,8 @@
 class Department < ApplicationRecord
     has_many :staffs, through: :join_staff_departments, 
-                        foreign_key: :staff_id, 
+                        foreign_key: :department_id, 
                         dependent: :destroy
     has_many :services, dependent: :destroy
     accepts_nested_attributes_for :services, reject_if: lambda {|attributes| attributes['content'].blank?}, allow_destroy: true
-    accepts_nested_attributes_for :staffs, allow_destroy: True
+    accepts_nested_attributes_for :staffs
 end
