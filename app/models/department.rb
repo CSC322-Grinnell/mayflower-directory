154f0755b614073774f27c9897e94d5b81889de7
tranle <lequytra@grinnell.edu>
2019-04-30 16:39:13 +0000

    Edit relationship model

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,5 +1,7 @@
 class Department < ApplicationRecord
-    has_many :staffs, through: :join_staff_departments
+    has_many :staffs, through: :join_staff_departments, 
+                        foreign_key: :staff_id, 
+                        dependent: :destroy
     has_many :services, dependent: :destroy
     accepts_nested_attributes_for :services, reject_if: lambda {|attributes| attributes['content'].blank?}, allow_destroy: true
     accepts_nested_attributes_for :staffs, allow_destroy: True
