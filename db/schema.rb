6201dbdd861ad3c03963c613f6afadcd653a28cb
tranle <lequytra@grinnell.edu>
2019-05-09 16:41:41 +0000

    Fix some end of file error in profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20190507133619) do
-
 
   create_table "departments", force: :cascade do |t|
     t.string   "name"
