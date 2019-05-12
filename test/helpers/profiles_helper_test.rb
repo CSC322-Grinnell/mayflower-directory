ebd1eaa6d5e6bcf8f9a147ffe70df2d9ca29df56
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2019-05-12 18:03:38 -0500

    Add tests for profiles helper methods

--- test/helpers/profiles_helper_test.rb
+++ test/helpers/profiles_helper_test.rb
@@ -1,6 +1,22 @@
 require "test_helper"
 
 class ProfilesHelperTest < ActionView::TestCase
+  test "full_name combines first name and last name" do
+    assert_equal(full_name(profiles(:sparse)), "Sparse No'Info")
+  end
+
+  test "full_name shows nickname and last name and parenthesizes first name" do
+    assert_equal(full_name(profiles(:frog)), "Froggy Green (Frog)")
+  end
+
+  test "avatar_url gives the cached avatar url if present" do
+    assert_equal(avatar_url(profiles(:frog)), "https://images.foo.com/froggo.jpg")
+  end
+
+  test "avatar_url gives the default avatar url if the cached isn't there" do
+    assert_equal(avatar_url(profiles(:sparse)), image_url("default_avatar.jpg"))
+  end
+
   test "normalize_line_breaks should change all repeated line breaks to 2 breaks" do
     assert_equal(
       normalize_line_breaks("foo\n\n\n\nbar\nbaz\n\nzing"),
