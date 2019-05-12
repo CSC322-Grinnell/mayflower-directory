952df49412c453ad17ae44d8bf349dece7ec8200
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2019-05-12 01:34:52 -0500

    Create s3 bucket lazily

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -7,47 +7,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :fog
 
-  def initialize(*args)
-    super
-    s3 = Aws::S3::Resource.new(
-      region: ENV['AWS_DEFAULT_REGION'],
-      credentials: Aws::Credentials.new(
-        ENV['AWS_ACCESS_KEY_ID'],
-        ENV['AWS_SECRET_ACCESS_KEY']
-      )
-    )
-
-    @bucket = s3.bucket('mayflower-data')
-  end
-
-  def file_exists?(filename)
-    @bucket.object(File.join(store_dir, filename)).exists?
-  end
-
-  def retrieve_for_profile!(profile)
-    if not profile.avatar.file.nil?
-      profile.avatar.url
-    else
-      begin
-        base_filename = "#{profile.last_name}, #{profile.first_name}"
-        png_filename = "#{base_filename}.png"
-        jpg_filename = "#{base_filename}.jpg"
-
-        if file_exists?(jpg_filename)
-          retrieve_from_store!(jpg_filename)
-          url
-        elsif file_exists?(png_filename)
-          retrieve_from_store!(png_filename)
-          url
-        else
-          nil
-        end
-      rescue Aws::S3::Errors::BadRequest
-        nil
-      end
-    end
-  end
-
   # Override the directory where uploaded files will be stored.
   def store_dir
     'images'
@@ -98,5 +57,51 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  def retrieve_for_profile!(profile)
+    if not profile.avatar.file.nil?
+      profile.avatar.url
+    else
+      begin
+        base_filename = "#{profile.last_name}, #{profile.first_name}"
+        png_filename = "#{base_filename}.png"
+        jpg_filename = "#{base_filename}.jpg"
+
+        if file_exists?(jpg_filename)
+          retrieve_from_store!(jpg_filename)
+          url
+        elsif file_exists?(png_filename)
+          retrieve_from_store!(png_filename)
+          url
+        else
+          nil
+        end
+      rescue Aws::S3::Errors::BadRequest
+        nil
+      end
+    end
+  end
+
+  private
+
+    def bucket
+      if not @bucket
+        s3 = Aws::S3::Resource.new(
+          region: ENV['AWS_DEFAULT_REGION'],
+          credentials: Aws::Credentials.new(
+            ENV['AWS_ACCESS_KEY_ID'],
+            ENV['AWS_SECRET_ACCESS_KEY']
+          )
+        )
+
+        @bucket = s3.bucket('mayflower-data')
+      end
+
+      @bucket
+    end
+
+    def file_exists?(filename)
+      bucket.object(File.join(store_dir, filename)).exists?
+    end
 
 end
